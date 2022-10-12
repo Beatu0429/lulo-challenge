c2d8ed7c27569162b59a80ea30e866326b185e21
Beatu0429 <nievesb0429@gmail.com>
2022-10-11 20:51:29 -0500

    first commit

---
+++ src/main/java/com/example/lulopruebatecnica/LuloPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lulopruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuloPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuloPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lulopruebatecnica/LuloPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lulopruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuloPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
